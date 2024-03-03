ec059322913d097c3cce313a504f513a31d8524d
glenacota <guido.lenacota@gmail.com>
2024-03-03 17:04:56 +0100

    add dirty test code

--- confluence_chunker/confluence_chunker.py
+++ confluence_chunker/confluence_chunker.py
@@ -1,1 +1,25 @@
 """Main module."""
+import os
+import logging
+
+from atlassian import Confluence
+from decouple import config
+from bs4 import BeautifulSoup
+
+# initialise logger
+log_level = os.environ.get("LOG_LEVEL", "INFO")
+logger = logging.getLogger(__name__)
+logger.setLevel(log_level)
+# init confluence lib
+confluence = Confluence(url="https://wiki.kreuzwerker.de/", token=config('CONFLUENCE_TOKEN'), timeout=300)
+
+response = confluence.get_page_by_id('137729483',expand="body.export_view")
+html_body = response['body']['export_view']['value']
+soup = BeautifulSoup(html_body, 'html.parser')
+for tag in soup.find_all(True):
+    tag.attrs = {}
+    for span in tag.find_all("span"):
+        span.unwrap()
+
+print(response['title'])
+print(soup.encode(formatter="html5"))
