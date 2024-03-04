70381c2604e9df2bcfbb343b6bf080fa7e684f06
glenacota <guido.lenacota@gmail.com>
2024-03-04 22:46:32 +0100

    support page children processing

--- confluence_chunker/confluence_chunker.py
+++ confluence_chunker/confluence_chunker.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 import logging
 import click
 import json
@@ -10,6 +10,7 @@ from lxml import etree, html
 
 # initialise logger
 log_level = os.environ.get("LOG_LEVEL", "INFO")
+logging.basicConfig(stream=sys.stdout, level=log_level)
 logger = logging.getLogger(__name__)
 logger.setLevel(log_level)
 
@@ -50,24 +51,33 @@ def map_chunk_to_json(confluence_rest_response, chunk):
         "chunk": chunk
     })
 
-def index_into_opensearch(index, chunks):
-    opensearch_client.indices.delete(index=index)
-    opensearch_client.indices.create(index=index, body=create_index_body)
-    [opensearch_client.index(index=index, body=chunk) for chunk in chunks]
+def index_into_opensearch(opensearch_index, chunks):
+    opensearch_client.indices.delete(index=opensearch_index, ignore_unavailable=True)
+    opensearch_client.indices.create(index=opensearch_index, body=create_index_body)
+    for loop_index, chunk in enumerate(chunks):
+        logger.info("Indexing '%s:' document %d/%d", opensearch_index, loop_index, len(chunks))
+        opensearch_client.index(index=opensearch_index, body=chunk)
 
-@click.command()
-@click.option('--pageid', prompt='Page id', default='137729483', help='The id of the wiki page to process.')
-@click.option('--method', prompt='Chunking method (none|nocontext|html)', default='none', help='The method applied by the chunkenizer.')  
-@click.option('--opensearch_index', help='The OpenSearch index whereto ingest the chunk data.')
-def run(pageid, method, opensearch_index):
+def get_chunks_from_page(pageid, method):
     response = confluence.get_page_by_id(pageid,expand="body.export_view")
     html_body = parse_html_body(response['body']['export_view']['value'])
     chunks = chunkenize_by_method(method, html_body)
     logger.info('Number of chunks created for page id %s: %s', pageid, len(chunks))
-    chunks_as_json = map_chunks_to_json(response, chunks)
+    return map_chunks_to_json(response, chunks)
 
-    if (opensearch_index):
-        index_into_opensearch(opensearch_index, chunks_as_json)
+@click.command()
+@click.option('--pageid', required=True, help='The id of the wiki page to process.')
+@click.option('--recursive', is_flag=True, default=False, help='Process all children (recursively) of the provided wiki page.')
+@click.option('--method', default='none', help='The method applied by the chunkenizer. Possible values: none|nocontext|html.')
+@click.option('--opensearch_index', help='The OpenSearch index whereto ingest the chunk data.')
+def run(pageid, recursive, method, opensearch_index):
+    chunks = get_chunks_from_page(pageid, method)
+    if (recursive):
+        child_ids = [response['id'] for response in confluence.get_page_child_by_type(pageid)]
+        for child_id in child_ids:
+            chunks.extend(get_chunks_from_page(child_id, method))
     
-    # print chunks
-    [print(chunk) for chunk in chunks_as_json]
+    if (opensearch_index):
+        index_into_opensearch(opensearch_index, chunks)
+    
+    [logger.info(chunk) for chunk in chunks]
